c01c590739953bcf6f70a1679401615ec7957be5
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-11-06 18:06:52 +0900

    add validation & parameters

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package sample
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // Hello just say "Hello"
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(("Hello")))
+	// Validate http method
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Only GET method is allowed"))
+	}
+	
+	// Parse query string
+	names, ok := r.URL.Query()["name"]
+	var name string
+	if !ok || len(names[0]) < 1 {
+		name = "World"
+	} else {
+		name = names[0]
+	}
+
+	msg := fmt.Sprintf("Hello %s", name)
+	w.Write([]byte(msg))
 }
