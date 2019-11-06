5579fc838417e9f84fba4ac000834509a12ce952
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-11-07 00:22:20 +0900

    add curl scripts

---
+++ curl-loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [[ ${1} = "" ]]
+then
+  echo "Usage: $0 <URL>"
+  exit
+fi
+
+readonly url=${1}
+
+for i in {0..100}; do
+    name=`sed -ne ${RANDOM}p /usr/share/dict/words`
+    curl ${url}?name=${name}
+done
