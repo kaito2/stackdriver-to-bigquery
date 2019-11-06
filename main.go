7237c07af8228eca2d81f77ae10c631e4625a203
Kaito Iwatsuki <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-11-06 23:32:54 +0900

    add logging library

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package sample
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"cloud.google.com/go/logging"
 )
+
+const project = "kaito2"
 
 // Hello just say "Hello"
 func Hello(w http.ResponseWriter, r *http.Request) {
-	// Validate http method
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Only GET method is allowed"))
+	ctx := r.Context()
+	// Get logging client
+	client, err := logging.NewClient(ctx, project)
+	if err != nil {
+		log.Fatal("Failed to get logging client")
 	}
-	
+
 	// Parse query string
 	names, ok := r.URL.Query()["name"]
 	var name string
@@ -20,6 +26,32 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 		name = "World"
 	} else {
 		name = names[0]
+	}
+
+	// Get logger
+	logger := client.Logger("sample-logger-1")
+
+	// logging
+	logger.Log(logging.Entry{
+		Payload:  name,
+		Severity: logging.Info,
+		Labels:   map[string]string{"bigquery-export-type": "sample"},
+	})
+
+	logger.Log(logging.Entry{
+		Payload:  "warning",
+		Severity: logging.Warning,
+	})
+
+	logger.Log(logging.Entry{
+		Payload:  "error",
+		Severity: logging.Error,
+	})
+
+	// Validate http method
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Only GET method is allowed"))
 	}
 
 	msg := fmt.Sprintf("Hello %s", name)
